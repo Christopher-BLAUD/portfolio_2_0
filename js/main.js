662715f08e9530c0ea83aebc8d1cc2cc9ae76f26
Christopher Blaud <christopher.blaud.esi@gmail.com>
2022-07-15 20:29:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+let typedHello = new Typed(".home_title_hello", {
+    strings: ["Hello,"],
+    typeSpeed: 60,
+    startDelay: 1000,
+    showCursor: false
+})
+
+let typedName = new Typed(".home_title_name", {
+    strings: ["Christopher Blaud"],
+    typeSpeed: 60,
+    startDelay: 2000,
+    showCursor: false
+})
+
+let typedJob = new Typed(".home_title_job", {
+    strings: ["je suis"],
+    typeSpeed: 60,
+    startDelay: 3800,
+    showCursor: false
+})
+
+let typed = new Typed(".auto-type", {
+    strings: ["Développeur web."],
+    typeSpeed: 60,
+    startDelay: 4400,
+    showCursor: false,
+    shuffle: false,
+})
